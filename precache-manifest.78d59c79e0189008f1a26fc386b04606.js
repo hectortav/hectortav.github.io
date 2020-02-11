40bf9f06eaaeecff441bfcdc33626e6e4952a09b
hectortav <tavhector@gmail.com>
2020-02-11 10:37:30 +0200

    Updates

---
+++ precache-manifest.78d59c79e0189008f1a26fc386b04606.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62bfbbfcc396025b78d43e5dc3b22d77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdd1627ae3be2fb8cd32",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ed69802585f8cd7d16ee",
+    "url": "/static/js/2.70d000f8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.70d000f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd1627ae3be2fb8cd32",
+    "url": "/static/js/main.d6a60e44.chunk.js"
+  },
+  {
+    "revision": "9181a59483de59913e87",
+    "url": "/static/js/runtime-main.37e8521d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
