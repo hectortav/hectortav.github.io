aadab23abd470f68cee404fb38393adb1d0d7627
hectortav <tavhector@gmail.com>
2021-02-11 16:46:33 +0200

    Add dynamic articles

---
+++ articles/article.js
@@ -0,0 +1,10 @@
+var data = {
+	test: {
+		title: "Title Title Title.",
+		subtitle: "Subtitle Subtitle Subtitle",
+		paragraphs: [
+			{ title: "Paragraph 1", text: "Text text text" },
+			{ title: "Paragraph 1", text: "Text text text" },
+		],
+	},
+};
