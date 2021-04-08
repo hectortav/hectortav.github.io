42cf19649abe6c6397113c949caadd67927e9c2d
hectortav <tavhector@gmail.com>
2021-04-08 22:00:54 +0300

    The burnout rumble.

--- articles/article.js
+++ articles/article.js
@@ -1,10 +1,24 @@
 var data = {
-	test: {
-		title: "Title Title Title.",
-		subtitle: "Subtitle Subtitle Subtitle",
+	burnout_rumble: {
+		title: "The burnout rumble.",
+		//subtitle: "",
 		paragraphs: [
-			{ title: "Paragraph 1", text: "Text text text" },
-			{ title: "Paragraph 1", text: "Text text text" },
+			{
+				title: "The past week I really feel burned out.",
+				text: `Working overtime, keeping up with university, working on side projects and of course lockdown suddenly feel way too much.
+Everything around me seems to be breaking down, the internet connection takes me back to the 90s and my monitor turns black for no reason. 
+Did I finally reach burnout, the inevitable phase every developer has to face at some point? 
+It kinda feels like it. I don't have the same energy for my side projects and I postpone my responsibilities.
+So what is the source of the problem?
+Probably lack of motivation is my main problem, whether that's a reason or an effect of burnout.
+I constantly have to work overtime and my effort is underappreciated since I work with non-developers. The API I am working on is really badly documented and stops working for non-apparent reasons. And of course as a younger developer I am significantly underpaid.
+Regarding university, I am stack with stupid math and physics problems that don't stimulate me anymore. Math is great, I hate it!
+Lastly, I  have to face my side projects, and especially needit.  I work as a solo developer and the humongous amount of work has overtaken me. Don't take me wrong, I like working on it and learning so much new stuff, but the work is simply too much for one person.`,
+			},
+			{
+				title: "I guess tomorrow is another day.",
+				text: `Work will get on track, my assignments will be finished in a week and my enthusiasm will kick back in a few days. Hopefully :P`,
+			},
 		],
 	},
 };
