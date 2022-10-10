d8e1f56b0bc07e8a50041ba9a01ce061c6b28512
hectortav <tavhector@gmail.com>
2022-10-10 10:00:48 +0300

    Change local time

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,7 +8,7 @@ const backImageSrc = "/assets/city.png";
 const Home = () => {
     const [time, setTime] = useState<string | null>(null);
     useEffect(() => {
-        getTime("Europe", "Brussels")
+        getTime("Europe", "Athens")
             .then((response: any) => {
                 const dateObj = JSON.parse(response);
                 const dateTime = dateObj.datetime;
@@ -20,33 +20,32 @@ const Home = () => {
     }, []);
     return (
         <div id="home" className="border-b border-slate-400">
-            <div className="flex flex-auto mt-20 flex-col sm:flex-row">
-                <div className="w-full sm:w-1/2 h-full flex flex-col min-h-[660px]">
+            <div className="mt-20 flex flex-auto flex-col sm:flex-row">
+                <div className="flex h-full min-h-[660px] w-full flex-col sm:w-1/2">
                     <h3 className="text-lg font-light">§ Home</h3>
-                    <h1 className="text-7xl font-semibold mt-10">Hey there!</h1>
-                    <h2 className="text-2xl font-normal mt-5">
+                    <h1 className="mt-10 text-7xl font-semibold">Hey there!</h1>
+                    <h2 className="mt-5 text-2xl font-normal">
                         My name is hector
                     </h2>
-                    <div className="text-xl font-light mt-30">
+                    <div className="mt-30 text-xl font-light">
                         I am a developer passionate about typescript and the
-                        web. I was born in Athens, Greece and based in Brussels,
-                        Belgium
+                        web. I am based in Athens, Greece
                         {time === null ? "." : `, the local time is ${time}.`}
                     </div>
                     <div className="mt-auto text-7xl">
                         <OrigamiBoat />
                     </div>
                 </div>
-                <div className="w-full sm:w-1/2 flex relative min-h-[400px] md:min-h-[660px] mt-10 md:mt-0 hover:rotate-1 transition ease-in-out delay-75">
+                <div className="relative mt-10 flex min-h-[400px] w-full transition delay-75 ease-in-out hover:rotate-1 sm:w-1/2 md:mt-0 md:min-h-[660px]">
                     <img
                         src={backImageSrc}
                         alt="city"
-                        className="origin-center -rotate-12 absolute insets-0 md:bottom-1/4 md:left-1/4 md:top-10 drop-shadow-xl w-[205px] lg:w-[307px] xl:w-[410px] translate-x-1/3 md:translate-x-0"
+                        className="insets-0 absolute w-[205px] origin-center translate-x-1/3 -rotate-12 drop-shadow-xl md:bottom-1/4 md:left-1/4 md:top-10 md:translate-x-0 lg:w-[307px] xl:w-[410px]"
                     />
                     <img
                         src={frontImageSrc}
                         alt="nature"
-                        className="origin-center rotate-6 absolute insets-0 md:left-1/4 drop-shadow-xl w-[200px] lg:w-[300px] xl:w-[400px] translate-x-1/3 lg:translate-x-0"
+                        className="insets-0 absolute w-[200px] origin-center translate-x-1/3 rotate-6 drop-shadow-xl md:left-1/4 lg:w-[300px] lg:translate-x-0 xl:w-[400px]"
                     />
                 </div>
             </div>
